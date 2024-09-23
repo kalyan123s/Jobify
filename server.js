9d5002f6aa2109967db8cc2f7c734e28329d50b2
Kalyan Sharma <kalyansharma855@gmail.com>
2024-09-23 12:11:49 +0530

    frontend fixed

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ cloudinary.config({
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname,'./public')))
+app.use(express.static(path.resolve(__dirname,'./client/dist')))
 
 
 if(process.env.NODE_ENV=== 'development'){
@@ -76,7 +76,7 @@ app.use('/api/v1/auth',authRouter);
 
 
 app.get('*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname, './public','index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist','index.html'));
 })
 
 // === NOT FOUND -> if all of the above request doesn't run===//
